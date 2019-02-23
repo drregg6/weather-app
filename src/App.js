ffc809f388b9df1053c3a35af227a5168604495f
Dave <drregg6@gmail.com>
2019-02-23 13:25:00 -0500

    basic API call, state updated, props passed to child

--- src/App.js
+++ src/App.js
@@ -3,19 +3,38 @@ import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Weather from './components/main/Weather';
 
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      city: '',
+      temp: 0,
+      description: ''
+    }
   }
+
+ componentDidMount() {
+    let city = 'Philadelphia';
+    const apikey = '&appid=780062c871d1cd69decae7b5869ddeba';
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}${apikey}`;
+    axios.get(endpoint)
+    .then(res => this.setState({
+      city: res.data.name,
+      temp: res.data.main.temp,
+      description: res.data.weather[0].description
+    }));
+    console.log(this.state);
+   }
 
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <Weather />
+          <Weather weather={this.state} />
           <Footer />
         </div>
       </div>
--- src/components/main/Weather.js
+++ src/components/main/Weather.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class Weather extends React.Component {
+    /*
+        let city = 'Philadelphia';
+        let lat = `lat=39&`;
+        let lon = `lon=139`;
+        const apikey = '&appid=780062c871d1cd69decae7b5869ddeba';
+        let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}${apikey}`;
+     */
+
     render() {
+        const { city, temp, description } = this.props.weather
         return (
             <div style={weatherStyle}>
-                <h1>Hello World!</h1>
+                <h1>{city}</h1>
+                <p>{temp}</p>
+                <p>{description}</p>
             </div>
         );
     }
 }
 
 const weatherStyle = {
-    height: '200px'
+    // height: '200px'
 }
 
 export default Weather;
